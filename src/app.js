50dc07498a4c0300158c13d4011bc7fa8f2a9a5b
8u1137pr00f <162195925+8u1137pr00f@users.noreply.github.com>
2024-05-31 08:48:35 +0000

    Primera entrega, todo funciona pero los palos superior e inferior no salen centrados y quiero corregirlo más adelante.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document
+  .getElementById("shuffleBtn")
+  .addEventListener("click", generateRandomCard);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateRandomCard() {
+  const suits = ["♠", "♥", "♦", "♣"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  const cardContainer = document.getElementById("cardContainer");
+  cardContainer.innerHTML = `
+        <div class="card">
+            <div class="top-left">${randomSuit}</div>
+            <div class="center">${randomValue}</div>
+            <div class="bottom-right">${randomSuit}</div>
+        </div>
+    `;
+
+  const color = randomSuit === "♥" || randomSuit === "♦" ? "red" : "black";
+  const cardElements = cardContainer.querySelectorAll(
+    ".card .top-left, .card .center, .card .bottom-right"
+  );
+  cardElements.forEach(element => {
+    element.style.color = color;
+  });
+}
